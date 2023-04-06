76619b6cd27c1a42edb9e4d7dee54892504c3225
BenjaminDGarrett <43711630+BenjaminDGarrett@users.noreply.github.com>
2023-04-05 19:57:08 -0500

    Add files via upload
    Midi GPT 4, No changes needed: Write a python script that generates a midi file that is in the style of Pete Namlook.

--- main.py
+++ main.py
@@ -1,70 +1,47 @@
-import pygame
+import mido
+from mido import MidiFile, MidiTrack, Message
 import random
-import time
 
-# Initialize Pygame
-pygame.init()
+def create_chord_progression():
+    chords = [
+        [48, 52, 55],  # C Major
+        [45, 48, 52],  # A Minor
+        [43, 47, 50],  # G Major
+        [41, 45, 48],  # E Minor
+    ]
+    progression = random.sample(chords, len(chords))
+    return progression
 
-# Define the size of the screen
-screen_width = 800
-screen_height = 600
+def create_melody(chord):
+    return random.choice(chord)
 
-# Create a new window
-screen = pygame.display.set_mode((screen_width, screen_height))
+def create_midi_file(filename, tempo=80):
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
 
-# Define the parameters of the moving object
-object_width = 50
-object_height = 50
-object_speed = 100  # pixels per second
+    track.append(Message('program_change', program=26, time=0))  # Synth pad
+    ticks_per_beat = mid.ticks_per_beat
+    ticks_per_measure = ticks_per_beat * 4
+    ticks_per_chord = ticks_per_measure // len(create_chord_progression())
 
-# Define a function to draw the object at a given position
-def draw_object(x, y):
-    # Create a new surface for the object
-    object_surface = pygame.Surface((object_width, object_height))
+    for chord in create_chord_progression():
+        for note in chord:
+            track.append(Message('note_on', note=note, velocity=64, time=0))
+            track.append(Message('note_off', note=note, velocity=64, time=ticks_per_chord))
 
-    # Fill the surface with a random color
-    object_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    object_surface.fill(object_color)
+    track.append(Message('program_change', program=1, time=0))  # Acoustic grand piano
+    for chord in create_chord_progression():
+        melody_note = create_melody(chord)
+        track.append(Message('note_on', note=melody_note, velocity=64, time=0))
+        track.append(Message('note_off', note=melody_note, velocity=64, time=ticks_per_chord))
 
-    # Draw a random pattern on the surface
-    for i in range(100):
-        pattern_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        pattern_x = random.randint(0, object_width)
-        pattern_y = random.randint(0, object_height)
-        pattern_size = random.randint(5, 20)
-        pygame.draw.circle(object_surface, pattern_color, (pattern_x, pattern_y), pattern_size)
+    track.append(Message('control_change', channel=0, control=64, value=127, time=0))  # Sustain pedal on
+    track.append(Message('control_change', channel=0, control=64, value=0, time=ticks_per_measure))  # Sustain pedal off
 
-    # Draw the surface at the given position
-    screen.blit(object_surface, (x, y))
+    mid.save(filename)
 
-# Define the starting position of the object
-object_x = 0
-object_y = 0
-
-# Loop forever
-while True:
-    # Handle Pygame events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-    # Clear the screen
-    screen.fill((0, 0, 0))
-
-    # Draw the object at the current position
-    draw_object(object_x, object_y)
-
-    # Update the display
-    pygame.display.flip()
-
-    # Wait for a short amount of time
-    time.sleep(0.1)
-
-    # Move the object to a new position
-    object_x += random.randint(-50, 50)
-    object_y += random.randint(-50, 50)
-
-    # Make sure the object stays within the boundaries of the screen
-    object_x = max(0, min(object_x, screen_width - object_width))
-    object_y = max(0, min(object_y, screen_height - object_height))
+if __name__ == "__main__":
+    filename = "Pete_Namlook_Style.mid"
+    create_midi_file(filename)
+    print(f"MIDI file '{filename}' has been created.")
